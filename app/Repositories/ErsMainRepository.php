64d9f59c02af821ef7754983fc565d76d93a5311
MohammedPrince <mohammed.beng@gmail.com>
2026-06-24 17:03:58 +0200

    Fixes in studentPayment

--- app/Repositories/ErsMainRepository.php
+++ app/Repositories/ErsMainRepository.php
@@ -229,7 +229,7 @@ class ErsMainRepository
                 return ['success' => false, 'code' => 400, 'message' => 'Invalid Date'];
             }
 
-            $paymentCheck = PaymentFib::where('student_index_no', $stud_id)->where('voucher', $voucher)->first();
+            $paymentCheck = PaymentFib::where('voucher', $voucher)->first();
             if ($paymentCheck || $viewData === 2) {
                 return ['success' => false, 'code' => 409, 'message' => 'Student already paid'];
             } else {
